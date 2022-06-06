9b687efb3ba32f2f6f8344c9a923cec68a67b0c0
AlbenaMitova <106745816+AlbenaMitova@users.noreply.github.com>
2022-06-06 10:15:35 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range lEgg')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
@@ -22,3 +22,6 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize (fruityvice_response.json())
 
 streamlit.dataframe(fruityvice_normalized )
+
+
+import snowflake.connector
